8b94f98e3132021fe8f641c1a5ef3844a8e45dc6
Sakib-Ansari <mohdsakibansari40@gamil.com>
2024-03-07 09:53:00 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let sections = document.querySelectorAll('section')
-let navlinks = document.querySelectorAll("header nav a")
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-bars');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
-        let height = sec.offsetHeigth;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
@@ -17,5 +25,5 @@ window.onscroll = () => {
     });
     
     let header = document.querySelector('header');
-      header.classList,toggle('sticky', window.scrollY > 100);
+      header.classList.toggle('sticky', window.scrollY > 100);
 };
